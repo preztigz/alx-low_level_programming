46eea7904e041532f4fd3519b6727b7c634490e6
Preztigz <ebojohprecious@gmail.com>
2023-06-22 11:47:40 -0700

    Print Numbers from 1-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 0; var <= 10; var++)
+		printf("%d", var);
+	printf('\n');
+}
