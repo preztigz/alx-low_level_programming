46388668c75af30e458cf57c24a6938da51e3a13
Preztigz <ebojohprecious@gmail.com>
2023-06-23 11:05:04 -0700

    0-Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
